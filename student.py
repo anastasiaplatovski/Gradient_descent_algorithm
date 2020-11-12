65ed73ab879b5c77acf3774e763ef936d86daffd
Anastasiia <47629075+kunashko@users.noreply.github.com>
2020-11-12 15:42:55 +0100

    Update student.py
    removed pandas requirements

--- student.py
+++ student.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-import pandas as pd
+import csv
 import argparse
 
 # create parser
 parser = argparse.ArgumentParser()
 
+# accept parameters from the command line
+parser.add_argument("--data")
+parser.add_argument("--eta")
+parser.add_argument("--threshold")
+
+args = parser.parse_args()
+
+def read_csv():
+    with open(args.data, newline = '') as f:
+        reader = csv.reader(f)
+        data = list(reader)
+    lst = []
+    for i in data:
+        for k in i:
+            lst.append(float(k))
+    data = [lst[i:i + 3] for i in range(0, len(lst), 3)]
+    return data
+
 def linear_regression():
-    # accept parameters from the command line
-    parser.add_argument("--data")
-    parser.add_argument("--eta")
-    parser.add_argument("--threshold")
-
-    args = parser.parse_args()
-
-    df = pd.read_csv(args.data)
+    df = read_csv()
     weight_0 = 0
     weight_1 = 0
     weight_2 = 0
@@ -26,19 +37,6 @@ def linear_regression():
     squared_sum_of_errors = 0
     while diff > threshold:
 
-        # if we decide to make the script more flexible, we can proceed as follows (this piece of code is for gradient, but we can adapt it for weights and Xs):
-        """
-        # retrieve the number of columns
-        grad = list(range(len(df.columns)))
-
-        # create a dictionary of variables gradient_
-        c = {str("gradient_" + str(i)): 0 for i in grad}
-
-        # create as many gradient values as columns: gradient_0 = 0, gradient_1 = 0, gradient_2 = 0
-        for k, v in c.items():
-            exec("%s=%s" % (k, v))
-        """
-
         squared_sum_of_errors_current = 0
         gradient_0 = 0
         gradient_1 = 0
@@ -46,9 +44,9 @@ def linear_regression():
 
         for i in range(len(df)):
             x_0 = 1
-            x_1 = df.iat[i, 0]
-            x_2 = df.iat[i, 1]
-            y = df.iat[i, 2]
+            x_1 = df[i][0]
+            x_2 = df[i][1]
+            y = df[i][2]
             y_current = weight_0 * x_0 + weight_1 * x_1 + weight_2 * x_2
             gradient_0 = gradient_0 + x_0 * (y - y_current)
             gradient_1 = gradient_1 + x_1 * (y - y_current)
