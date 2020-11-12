4a8a23855ae6128d19dff4f6e4fc6be30fc56f12
anastasiiaplatovskikh <mplat.63@gmail.com>
2020-11-12 16:01:45 +0100

    renamed variable learning rate

--- student.py
+++ student.py
@@ -14,8 +14,9 @@ parser.add_argument("--threshold")
 
 args = parser.parse_args()
 
+
 def read_csv():
-    with open(args.data, newline = '') as f:
+    with open(args.data, newline='') as f:
         reader = csv.reader(f)
         data = list(reader)
     lst = []
@@ -25,13 +26,14 @@ def read_csv():
     data = [lst[i:i + 3] for i in range(0, len(lst), 3)]
     return data
 
+
 def linear_regression():
     df = read_csv()
     weight_0 = 0
     weight_1 = 0
     weight_2 = 0
     iteration_number = 0
-    learningRate = float(args.eta)
+    learning_rate = float(args.eta)
     threshold = float(args.threshold)
     diff = 1
     squared_sum_of_errors = 0
@@ -52,14 +54,16 @@ def linear_regression():
             gradient_1 = gradient_1 + x_1 * (y - y_current)
             gradient_2 = gradient_2 + x_2 * (y - y_current)
             squared_sum_of_errors_current = squared_sum_of_errors_current + ((y - y_current) ** 2)
-        print(str(iteration_number)+","+str(round(weight_0, 4))+","+str(round(weight_1, 4))+","+str(round(weight_2, 4))+","+
+        print(str(iteration_number) + "," + str(round(weight_0, 4)) + "," + str(round(weight_1, 4)) + "," + str(
+            round(weight_2, 4)) + "," +
               str(round(squared_sum_of_errors_current, 4)))
         diff = abs(squared_sum_of_errors - squared_sum_of_errors_current)
         squared_sum_of_errors = squared_sum_of_errors_current
-        weight_0 = weight_0 + learningRate * gradient_0
-        weight_1 = weight_1 + learningRate * gradient_1
-        weight_2 = weight_2 + learningRate * gradient_2
+        weight_0 = weight_0 + learning_rate * gradient_0
+        weight_1 = weight_1 + learning_rate * gradient_1
+        weight_2 = weight_2 + learning_rate * gradient_2
 
         iteration_number = iteration_number + 1
+
 
 linear_regression()
